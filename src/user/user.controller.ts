9556d563a833638156ebc6799cc0057e1b5b60d7
tipakorn11 <55394461+tipakorn11@users.noreply.github.com>
2024-10-10 23:04:23 +0700

    update

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,37 +1,39 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-import {loginUserDto} from './dto/login-user.dto'
+import { loginUserDto } from './dto/login-user.dto'
+import { AuthGuard } from '../middleware/bearer-guard'
+
 @Controller('api/v1/user')
 export class UserController {
-  constructor(private readonly userService: UserService) {}
-
+  constructor(private readonly userService: UserService) { }
+  @Post('/login')
+  login(@Body() loginUserDto: loginUserDto) {
+    return this.userService.login(loginUserDto)
+  }
+  @UseGuards(AuthGuard)
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
     return this.userService.create(createUserDto);
   }
 
-  @Post('/login')
-    login(@Body() loginUserDto:loginUserDto){
-      return this.userService.login(loginUserDto)
-    }
-
+  @UseGuards(AuthGuard)
   @Get()
   findAll() {
     return this.userService.findAll();
   }
-
+  @UseGuards(AuthGuard)
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.userService.findOne(+id);
   }
-
+  @UseGuards(AuthGuard)
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.userService.update(+id, updateUserDto);
   }
-
+  @UseGuards(AuthGuard)
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.userService.remove(+id);
